b65b6f985da9c60adf1008aaaec4f77da11c2b6a
Qiao Han <qiao@supabase.io>
2025-11-05 13:10:27 +0800

    chore: add function with nested imports

---
+++ supabase/functions/shared/mod.ts
@@ -0,0 +1,10 @@
+export const handler = async (req) => {
+  const { name } = await req.json();
+  const data = {
+    message: `Hello ${name}!`,
+  };
+
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+  });
+};
